d09af8e650eb5608460119b1778ed41840e5655b
Babushkin Ilya <78734809+babushkin-designer@users.noreply.github.com>
2025-07-16 16:47:41 +0700

    Update page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+// app/page.tsx
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen bg-white">
+      <h1 className="text-4xl font-bold">kate shu</h1>
+    </main>
+  )
+}
